3898b0ce2d059dd5df1c183c90886eb1a0b52803
sfrankenthaler <77997141+sfrankenthaler@users.noreply.github.com>
2021-11-30 17:19:31 -0600

    Add files via upload

---
+++ microsite/fully-realized/scripts/scripts.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {			/* Here we're using JQuery to check that the DOM (the document) is ready */
+			    
+$(".controls").click(function(){		/* This line says to listen for a "click" event on something with with the class of "controls" */
+$("#drawer").toggleClass("open");	/* A click happened, so now we toggle (add/remove) the class "open" on an element with the ID of "drawer" */
+console.log("Toggled the class!");	/* For help troubleshooting, we're logging a note to the browser console so we can see that the function ran */
+});
+
+});
+
+
